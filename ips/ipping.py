3b3486ce1ba535d2d45ce649ef3cfbb606a18992
xshrim <xshrim@yeah.net>
2017-06-02 10:17:27 +0800

    小程序

--- ips/ipping.py
+++ ips/ipping.py
@@ -7,6 +7,7 @@ import getopt
 import platform
 import subprocess
 
+
 class cip:
     address = ''
     packetloss = ''
@@ -15,6 +16,7 @@ class cip:
     avgdelay = ''
     output = ''
     status = ''
+
     def __init__(self, address, packetloss, mindelay, maxdelay, avgdelay, output, status):
         self.address = address
         self.packetloss = packetloss
@@ -52,7 +54,7 @@ def showIP():
             cmd = 'ifconfig -a && route -n'
         if syspf == 'windows':
             cmd = 'ipconfig /all'
-        p = subprocess.Popen(cmd, stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell = True)
+        p = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
         output = p.stdout.read()
         return output.decode('gbk')
     except Exception as ex:
@@ -69,7 +71,7 @@ def getIPS(ipda, ipdb):
         ipda, ipdb = ipdb, ipda
     starta, startb, startc, startd = ipda.split('.')
     enda, endb, endc, endd = ipdb.split('.')
-    for ipa in range(int(starta), int(enda) +1):
+    for ipa in range(int(starta), int(enda) + 1):
         startrange = 0
         endrange = 255
         if ipa == int(starta):
@@ -80,14 +82,14 @@ def getIPS(ipda, ipdb):
             startrange = 0
             endrange = 255
             if ipa == int(starta) and ipb == int(startb):
-                startrange= int(startc)
+                startrange = int(startc)
             if ipa == int(enda) and ipb == int(endb):
                 endrange = int(endc)
             for ipc in range(startrange, endrange + 1):
                 for ipd in range(1, 255):
                     newip = '.'.join([str(ipa), str(ipb), str(ipc), str(ipd)])
                     if ipda <= convertIP(newip) <= ipdb:
-                        #print(newip)
+                        # print(newip)
                         ips.append(newip)
     return ips
 
@@ -103,14 +105,14 @@ def getPING(domain, times=2, timeout=200):
         if syspf == 'linux':
             cmd = 'ping -c ' + str(times) + ' -w ' + str(timeout) + ' ' + str(domain)
         if syspf == 'windows':
-            cmd = 'ping.exe -n ' + str(times) + ' -w '+ str(timeout) + ' ' + str(domain)
-        p = subprocess.Popen(cmd, stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell = True)
+            cmd = 'ping.exe -n ' + str(times) + ' -w ' + str(timeout) + ' ' + str(domain)
+        p = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
         output = p.stdout.read()
         if syspf == 'windows':
             output = output.decode('gbk')
-            regIP = r'\d+\.\d+\.\d+\.\d+'   ## Pinging www.a.shifen.com [115.239.211.112] with 32 bytes of data
-            regLost = r'(.*?)(\d+%)(.*)' ## Packets: Sent = 4, Received = 4, Lost = 0 (0% loss)   数据包: 已发送 = 4，已接收 = 4，丢失 = 0 (0% 丢失)，
-            regMinimum = r'Minimum = \d+ms|最短 = \d+ms'## Minimum = 37ms, Maximum = 38ms, Average = 37ms   最短 = 37ms，最长 = 77ms，平均 = 48ms
+            regIP = r'\d+\.\d+\.\d+\.\d+'   # # Pinging www.a.shifen.com [115.239.211.112] with 32 bytes of data
+            regLost = r'(.*?)(\d+%)(.*)'  # # Packets: Sent = 4, Received = 4, Lost = 0 (0% loss)   数据包: 已发送 = 4，已接收 = 4，丢失 = 0 (0% 丢失)，
+            regMinimum = r'Minimum = \d+ms|最短 = \d+ms'  # # Minimum = 37ms, Maximum = 38ms, Average = 37ms   最短 = 37ms，最长 = 77ms，平均 = 48ms
             regMaximum = r'Maximum = \d+ms|最长 = \d+ms'
             regAverage = r'Average = \d+ms|平均 = \d+ms'
             ipaddr = re.search(regIP, output)
@@ -124,19 +126,19 @@ def getPING(domain, times=2, timeout=200):
                 lost = lost.group(2)
             if minimum is not None:
                 minimum = re.search(r'(.*?)(\d+)(.*)', minimum.group()).group(2)
-                #minimum = ''.join(filter(lambda x:x.isdigit(),minimum.group()))
+                # minimum = ''.join(filter(lambda x:x.isdigit(),minimum.group()))
             if maximum is not None:
                 maximum = re.search(r'(.*?)(\d+)(.*)', maximum.group()).group(2)
-                #maximum = ''.join(filter(lambda x:x.isdigit(),maximum.group()))
+                # maximum = ''.join(filter(lambda x:x.isdigit(),maximum.group()))
             if average is not None:
                 average = re.search(r'(.*?)(\d+)(.*)', average.group()).group(2)
-                #average = ''.join(filter(lambda x:x.isdigit(),average.group()))
+                # average = ''.join(filter(lambda x:x.isdigit(),average.group()))
         if syspf == 'linux':
             output = output.decode('gbk')
             regIP = r'\d+\.\d+\.\d+\.\d+'
             regLost = r'(.*?)(\d+%)(.*)'
             regmum = r'(min/avg/max/mdev = )(.*)ms'
-            #print(output)
+            # print(output)
             ipaddr = re.search(regIP, output)
             lost = re.search(regLost, output)
             mum = re.search(regmum, output)
@@ -147,10 +149,10 @@ def getPING(domain, times=2, timeout=200):
             if mum is not None:
                 mums = str(mum.group(2)).replace(' ', '')
                 minimum, maximum, average = mums.split('/')[:3]
-        #print(average.group())
+        # print(average.group())
         return [str(ipaddr), str(lost), str(minimum), str(maximum), str(average), str(output)]
     except Exception as ex:
-        #print(ex)
+        # print(ex)
         return [str(domain), '100%', 'None', 'None', 'None', str(ex)]
 
 
@@ -162,7 +164,7 @@ def testIP(iplist, times=2, timeout=200, rtop=True):
                 if rtop:
                     print('Testing ' + ipaddr, end=' ... ')
                 res = getPING(ipaddr, times, timeout)
-                #print(res)
+                # print(res)
                 if res[1] == 'None':
                     if rtop:
                         print('[NotFound]')
@@ -189,7 +191,7 @@ def testIPS(startip, endip, times=2, timeout=200, rtop=True):
             if rtop:
                 print('Testing ' + ipaddr, end=' ... ')
             res = getPING(ipaddr, times, timeout)
-            #print(res)
+            # print(res)
             if res[1] == 'None':
                 if rtop:
                     print('[NotFound]')
@@ -216,13 +218,13 @@ def main(argv):
         try:
             opts, args = getopt.getopt(argv, "hf:t:", ["from=", "to="])
         except getopt.GetoptError:
-            print ('Usage  : ipp.py [-f <startip>] [-t <endip>] [<ipaddr>]')
+            print('Usage  : ipp.py [-f <startip>] [-t <endip>] [<ipaddr>]')
             exit(2)
         if len(args) > 0:
             testIP(args)
         for opt, arg in opts:
             if opt == '-h':
-                print ('Usage  : ipp.py [-f <startip>] [-t <endip>] [<ipaddr>]')
+                print('Usage  : ipp.py [-f <startip>] [-t <endip>] [<ipaddr>]')
                 exit()
             elif opt in ("-f", "--from"):
                 startip = arg
@@ -234,6 +236,3 @@ def main(argv):
 
 if __name__ == '__main__':
     main(sys.argv[1:])
-
-
-
