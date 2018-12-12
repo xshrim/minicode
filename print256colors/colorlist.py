66ee1f30f24665048c271886d2c5e72be89501c2
xshrim <xshrim@gmail.com>
2018-12-12 15:50:29 +0800

    minicode

--- print256colors/colorlist.py
+++ print256colors/colorlist.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
+import sys
 # Ported to Python from http://www.vim.org/scripts/script.php?script_id=1349
 
 print("Color indexes should be drawn in bold text of the same color.")
 
 colored = [0] + [0x5f + 40 * n for n in range(0, 5)]
 colored_palette = [
-	"%02x/%02x/%02x" % (r, g, b) 
+	"%02x/%02x/%02x" % (r, g, b)
 	for r in colored
 	for g in colored
 	for b in colored
@@ -14,15 +15,21 @@ colored_palette = [
 grayscale = [0x08 + 10 * n for n in range(0, 24)]
 grayscale_palette = [
 	"%02x/%02x/%02x" % (a, a, a)
-	for a in grayscale 
+	for a in grayscale
 ]
 
-normal = "\033[38;5;%sm" 
+normal = "\033[38;5;%sm"
 bold = "\033[1;38;5;%sm"
 reset = "\033[0m"
 
 for (i, color) in enumerate(colored_palette + grayscale_palette, 16):
-	index = (bold + "%4s" + reset) % (i, str(i) + ':')
-	hex   = (normal + "%s" + reset) % (i, color)
-	newline = '\n' if i % 6 == 3 else '  '
-	print(index + '  ' + hex + newline, end='')
+    index = (bold + "%4s" + reset) % (i, str(i) + ':')
+    hex   = (normal + "%s" + reset) % (i, color)
+    newline = '\n' if i % 6 == 3 else '  '
+    if len(sys.argv) > 1:
+        if color.replace('/', '').strip() == str(sys.argv[1]).replace('#', '').replace('/', '').replace(' ', '').strip().lower():
+            print(index + '  ' + hex)
+            break
+    else:
+        print(index + '  ' + hex + newline, end='')
+
