4683e5960e1e02f90e663bac833818dad82e61ee
xshrim <xshrim@gmail.com>
2020-11-03 10:09:13 +0800

    update

--- xbox/artifacts/.oh-my-zsh/custom/plugins/comm/comm.plugin.zsh
+++ xbox/artifacts/.oh-my-zsh/custom/plugins/comm/comm.plugin.zsh
@@ -659,6 +659,7 @@ alias ss='ss -asntup'
 alias open='xdg-open'
 alias grep='grep --color=auto'
 alias glog='git log --oneline --graph --decorate'
+alias glg='git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative'
 alias rs='rsync -ahr --info=progress2 --no-i-r'
 alias gm='gomacro'
 # docker alias
